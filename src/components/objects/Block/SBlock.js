eedb28155c3a58b8e3b5ee744514cbdfabf700ab
drewcurran <denisac@princeton.edu>
2023-12-12 16:48:04 -0500

    super

--- src/components/objects/Block/SBlock.js
+++ src/components/objects/Block/SBlock.js
@@ -16,67 +16,8 @@ class SBlock extends Shape {
     }
 
     action(event){
-        let performAction = true;
-        if (event.code === "ArrowRight"){
-            for(const block of this.items){
-                if (block.position.x > 3 || block.locked || block.checkCollision(block, 1, 0, 0)) {
-                    performAction = false;
-                }
-            }
-            if (performAction) {
-                for(const block of this.items){
-                    block.position.x += 2;
-                }
-            }
-        }
-        if (event.code === "ArrowLeft"){
-            for(const block of this.items){
-                if (block.position.x < -3 || block.locked || block.checkCollision(block, -1, 0, 0)) {
-                    performAction = false;
-                }
-            }
-            if (performAction) {
-                for(const block of this.items){
-                    block.position.x -= 2;
-                }
-            }
-        }
-        if (event.code === "ArrowDown"){
-            for(const block of this.items){
-                if (block.position.z > -3 || block.locked || block.checkCollision(block, 0, 0, 1)) {
-                    performAction = false;
-                }
-            }
-            if (performAction) {
-                for(const block of this.items){
-                    block.position.z += 2;
-                }
-            }
-        }
-        if (event.code === "ArrowUp"){
-            for(const block of this.items){
-                if (block.position.z < -9 || block.locked || block.checkCollision(block, 0, 0, -1)) {
-                    performAction = false;
-                }
-            }
-            if (performAction) {
-                for(const block of this.items){
-                    block.position.z -= 2;
-                }
-            }
-        }
-        if (event.code === "Space"){
-            for(const block of this.items){
-                if (block.position.y < -2 || block.locked || block.checkCollision(block, 0, -1, 0)) {
-                    performAction = false;
-                }
-            }
-            if (performAction) {
-                for(const block of this.items){
-                    block.position.y -= 2;
-                }
-            }
-        }
+        super.action(event);
+        
         if (event.code === "KeyQ") {
             let willCollde = false;
             if (this.orientation == 0){
