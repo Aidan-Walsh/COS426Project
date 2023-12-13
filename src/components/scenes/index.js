1ac0a10c6cceb995f93ad3d5453a23726a133520
Gavin Leising <gleising@princeton.edu>
2023-12-13 17:53:33 -0500

    Super Levels

--- src/components/scenes/index.js
+++ src/components/scenes/index.js
@@ -1,4 +1,5 @@
 export { default as SeedScene } from './SeedScene.js';
+export { default as Sandbox } from './Sandbox.js';
 export { default as LevelOne } from './LevelOne.js';
 export { default as LevelTwo } from './LevelTwo.js';
 export { default as LevelThree } from './LevelThree.js';
